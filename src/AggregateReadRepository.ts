e406c8d2568df730b8c1576fd3b9625821cf1b62
Michael Bauer <mbauer.mphil@googlemail.com>
2023-05-30 02:44:38 +0200

    improve typing in AggregateReadRepository; set version to v0.7.4

--- src/AggregateReadRepository.ts
+++ src/AggregateReadRepository.ts
@@ -15,34 +15,35 @@ import {
 	isSnapshotDomainEvent,
 } from './DomainEvent.js';
 
-export class AggregateReadRepository<AggregateType extends string> {
-	private readonly aggregateOptionsById = new Map<string, Optional<Aggregate<AggregateType, unknown>>>();
+export class AggregateReadRepository<AggregateTypeName extends string, T extends Aggregate<AggregateTypeName, unknown>> {
+	private readonly aggregateOptionsById = new Map<string, Optional<Aggregate<AggregateTypeName, unknown>>>();
 
-	constructor(public readonly aggregateType: AggregateType, private readonly eventStorage: EventStorage) {}
+	constructor(public readonly aggregateType: AggregateTypeName, private readonly eventStorage: EventStorage) {}
 
-	get(id: string): Task<Error, Aggregate<AggregateType, unknown>> {
+	get(id: string): Task<Error, T> {
 		const aggregateOptionFromStorage = this.aggregateOptionsById.get(id);
 		if (aggregateOptionFromStorage?.isSome()) {
-			return this.updateAggregateFromStorage(aggregateOptionFromStorage.getOrThrow(() => new Error('Aggregate option was none.')))
+			return this.updateAggregateFromStorage(aggregateOptionFromStorage.getOrThrow(() => new Error('Aggregate option was none.')) as T)
 				.map(aggregate => {
-					this.aggregateOptionsById.set(id, new Some<Aggregate<AggregateType, unknown>>(aggregate));
+					this.aggregateOptionsById.set(id, new Some<T>(aggregate));
 					return aggregate;
 				});
 		}
 
 	    const getAggregateTask = this.getAggregateFromStorage(id);
 		const savingGetAggregateTask = getAggregateTask.map(aggregate => {
-			this.aggregateOptionsById.set(id, new Some<Aggregate<AggregateType, unknown>>(aggregate));
+			this.aggregateOptionsById.set(id, new Some<T>(aggregate));
 			return aggregate;
 		});
+
 		return savingGetAggregateTask;
 	}
 
-	private updateAggregateFromStorage(aggregate: Aggregate<AggregateType, unknown>): Task<Error, Aggregate<AggregateType, unknown>> {
+	private updateAggregateFromStorage(aggregate: T): Task<Error, T> {
 		// Get all events for versions after the current one, then apply them to the aggregate.
 		const noneDateRange: None<DateRange> = None.for<DateRange>();
 		const versionRange = new VersionRange(aggregate.version, undefined, false, false);
-		const filterData: AggregateEventFilterData<AggregateType> = {
+		const filterData: AggregateEventFilterData<AggregateTypeName> = {
 			type: this.aggregateType,
 			dateRange: noneDateRange,
 			aggregateId: new Some<string>(aggregate.id),
@@ -50,51 +51,51 @@ export class AggregateReadRepository<AggregateType extends string> {
 		};
 
 		const eventsToApplyTask = this.eventStorage.produceEvents(filterData);
-		return eventsToApplyTask.flatMap(generator => {
+		return eventsToApplyTask.flatMap<T>(generator => {
 			const eventsToApply = Array.from(generator);
-			return aggregate.withAppliedEvents(eventsToApply as Array<BasicDomainEvent<AggregateType, unknown, any>>);
+			return aggregate.withAppliedEvents(eventsToApply as Array<BasicDomainEvent<AggregateTypeName, unknown, any>>) as Task<Error, T>;
 		});
 	}
 
-	private getAggregateFromStorage(id: string): Task<Error, Aggregate<AggregateType, unknown>> {
+	private getAggregateFromStorage(id: string): Task<Error, T> {
 		const noneDateRange: None<DateRange> = None.for<DateRange>();
 		const noneVersionRange: None<VersionRange> = None.for<VersionRange>();
 		// eslint-disable-next-line @typescript-eslint/no-this-alias, unicorn/no-this-assignment
 		const thisAlias = this;
-		const filterData: AggregateEventFilterData<AggregateType> = {
+		const filterData: AggregateEventFilterData<AggregateTypeName> = {
 			type: this.aggregateType,
 			dateRange: noneDateRange,
 			aggregateId: new Some<string>(id),
 			versionRange: noneVersionRange,
 		};
-		return this.eventStorage.produceEvents(filterData).flatMap<Aggregate<AggregateType, unknown>>(
-			(gen): Task<Error, Aggregate<AggregateType, unknown>> => {
+		return this.eventStorage.produceEvents(filterData).flatMap<T>(
+			(gen): Task<Error, T> => {
 				function getFromCompleteEventStream() {
-					return thisAlias.handleCompleteEventStream(gen as Generator<Event<AggregateType, any>>, id);
+					return thisAlias.handleCompleteEventStream(gen as Generator<Event<AggregateTypeName, any>>, id);
 				}
 
 				const consumeTask
 					= new Task<
 					Error,
 					[
-						(InitializingDomainEvent<AggregateType, unknown, any> | SnapshotDomainEvent<AggregateType, unknown, any>),
-						Array<BasicDomainEvent<AggregateType, unknown, any>>,
+						(InitializingDomainEvent<AggregateTypeName, unknown, any> | SnapshotDomainEvent<AggregateTypeName, unknown, any>),
+						Array<BasicDomainEvent<AggregateTypeName, unknown, any>>,
 					]>(getFromCompleteEventStream);
 
 				return consumeTask.flatMap(tuple => {
 					const [baseEvent, otherEvents] = tuple;
-					return baseEvent.getSnapshot().withAppliedEvents(otherEvents);
+					return baseEvent.getSnapshot().withAppliedEvents(otherEvents) as Task<Error, T>;
 				});
 			},
 		);
 	}
 
 	private handleCompleteEventStream(
-		invokedGen: Generator<Event<AggregateType, any>>,
+		invokedGen: Generator<Event<AggregateTypeName, any>>,
 		id: string,
-	): Either<Error, [(InitializingDomainEvent<AggregateType, unknown, any> | SnapshotDomainEvent<AggregateType, unknown, any>), Array<BasicDomainEvent<AggregateType, unknown, any>>]> {
-		let base: (InitializingDomainEvent<AggregateType, unknown, any> | SnapshotDomainEvent<AggregateType, unknown, any>) | undefined;
-		const otherEvents: Array<BasicDomainEvent<AggregateType, unknown, any>> = [];
+	): Either<Error, [(InitializingDomainEvent<AggregateTypeName, unknown, any> | SnapshotDomainEvent<AggregateTypeName, unknown, any>), Array<BasicDomainEvent<AggregateTypeName, unknown, any>>]> {
+		let base: (InitializingDomainEvent<AggregateTypeName, unknown, any> | SnapshotDomainEvent<AggregateTypeName, unknown, any>) | undefined;
+		const otherEvents: Array<BasicDomainEvent<AggregateTypeName, unknown, any>> = [];
 		for (const event of invokedGen) {
 			if (isDomainEvent(event)) {
 				if (isInitializingDomainEvent(event) || isSnapshotDomainEvent(event)) {
